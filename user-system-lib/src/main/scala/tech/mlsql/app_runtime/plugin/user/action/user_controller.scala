f7bd598589080334c1cfcdb03c5e5dd2e6e9d915
WilliamZhu <allwefantasy@gmail.com>
2020-01-31 21:55:52 +0800

    udpate

--- user-system-lib/src/main/scala/tech/mlsql/app_runtime/plugin/user/action/user_controller.scala
+++ user-system-lib/src/main/scala/tech/mlsql/app_runtime/plugin/user/action/user_controller.scala
@@ -11,14 +11,14 @@ import tech.mlsql.app_runtime.plugin.user.quill_model.{User, UserSessionDB}
 import tech.mlsql.app_runtime.plugin.user.{PluginDB, Session, SystemConfig}
 import tech.mlsql.common.utils.Md5
 import tech.mlsql.common.utils.serder.json.JSONTool
-import tech.mlsql.serviceframework.platform.action.CustomAction
+import tech.mlsql.serviceframework.platform.action.{ActionContext, CustomAction}
 import tech.mlsql.serviceframework.platform.{PluginItem, PluginType}
 
 
 class UserReg extends CustomAction {
 
   override def run(params: Map[String, String]): String = {
-    if (!UserService.isEnableReg) JSONTool.toJsonStr(List(Map("msg" -> "User register is not enabled")))
+    if (!UserService.isEnableReg) return JSONTool.toJsonStr(List(Map("msg" -> "User register is not enabled")))
 
     UserService.findUser(params("name")) match {
       case Some(_) => JSONTool.toJsonStr(List(Map("msg" -> s"User ${params("name")} is exits")))
@@ -51,7 +51,6 @@ class UserLogin extends CustomAction {
 class IsLogin extends CustomAction {
 
   override def run(params: Map[String, String]): String = {
-    println("---------------##xxxx#---------------------")
     val tokenOpt = params.get("token")
     val passwordOpt = params.get("password")
     val name = params("name")
@@ -71,6 +70,7 @@ class IsLogin extends CustomAction {
         tokenMatch(token)
       case (Some(token), Some(password)) =>
         tokenMatch(token)
+      case (None, None) => List()
 
     }
 
@@ -85,7 +85,13 @@ class UserQuery extends CustomAction {
         case Some(user) => List(user.copy(password = ""))
         case None => List()
       }
-      case None => ctx.run(ctx.query[User]).toList.map(f => f.name)
+      case None =>
+        val context = ActionContext.context()
+        val notLogin = JSONTool.parseJson[List[Session]](new IsLogin().run(params)).isEmpty
+        if (notLogin) {
+          render(context.httpContext.response, 400, "login required")
+        }
+        ctx.run(ctx.query[User]).toList.map(f => f.name)
     }
     JSONTool.toJsonStr(items)
   }
@@ -95,6 +101,10 @@ class UserQuery extends CustomAction {
 
 class EnableOrDisableQuery extends CustomAction {
   override def run(params: Map[String, String]): String = {
+    val context = ActionContext.context()
+    if (!BasicDBService.canAccess(params("admin_token")))
+      render(context.httpContext.response, 400, "admin_token is required")
+
     val value = if (params("enable").toBoolean) SystemConfig.REG_ENABLE.toString
     else SystemConfig.REG_DISABLE.toString
     BasicDBService.addItem(SystemConfig.REG_KEY.toString, value, DictType.SYSTEM_CONFIG)
